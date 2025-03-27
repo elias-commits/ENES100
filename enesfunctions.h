9135f83dc5b531da3119082139e754dfda50f198
elias-commits <enelson6@terpmail.umd.edu>
2025-03-27 17:13:56 -0400

    Update enesfunctions.h
    fixed read_distance()

--- enesfunctions.h
+++ enesfunctions.h
@@ -1,22 +1,25 @@
-#include "enesmacros.h"
+#include "enesheaders.h"
+
+float distance = 0.;
+float duration = 0.;
 
 float read_distance() {
-    float distance = 0.;
-    float duration = 0.;
     digitalWrite(ULTRASONIC_TRIGGER,LOW);
     delayMicroseconds(2);
     digitalWrite(ULTRASONIC_TRIGGER,HIGH);
     delayMicroseconds(10);
-  
+    digitalWrite(ULTRASONIC_TRIGGER,LOW);
+
     duration = pulseIn(ULTRASONIC_ECHO,HIGH);
     distance = (duration*0.343)/2;
+
     return distance;
 }
 
 // add a function to calculate speed from PWM?
 // currently takes raw PWM value
 
-void turn_left(int speed) {
+void drive_left(int speed) {
     analogWrite(BOTH_MOTORS_PWM,speed);
     digitalWrite(R_MOTOR_DIRECTION,0);
     digitalWrite(L_MOTOR_DIRECTION,1);
@@ -25,11 +28,11 @@ void turn_left(int speed) {
 
 void drive_forward(int speed) {
     analogWrite(BOTH_MOTORS_PWM,speed);
-    digitalWrite(R_MOTOR_DIRECTION,0);
+    digitalWrite(R_MOTOR_DIRECTION,1);
     digitalWrite(L_MOTOR_DIRECTION,1);
 }
 
-void turn_right(int speed) {
+void drive_right(int speed) {
     analogWrite(BOTH_MOTORS_PWM,speed);
     digitalWrite(R_MOTOR_DIRECTION,1);
     digitalWrite(L_MOTOR_DIRECTION,0);
