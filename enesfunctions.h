7ff86b3fa7b3dee2f64bf5e1c9cd195725c7ce3b
elias-commits <enelson6@terpmail.umd.edu>
2025-04-15 15:43:31 -0400

    Update enesfunctions.h
    Added go_to_coords function

--- enesfunctions.h
+++ enesfunctions.h
@@ -1,7 +1,11 @@
 #include "enesheaders.h"
+#include <math.h>
+#include <Enes100.h>
 
 float distance = 0.;
 float duration = 0.;
+
+
 
 struct RGB {
   float r;
@@ -94,6 +98,10 @@ void drive_forwards(int speed) {
     left_forwards();
 }
 
+void stop_moving() {
+  drive_forwards(0);
+}
+
 void drive_backwards(int speed) {
     analogWrite(BOTH_MOTORS_PWM,speed);
     right_backwards();
@@ -112,6 +120,50 @@ void drive_right(int speed) {
     right_forwards();
     left_backwards();
 }
+
+void turn(double newAngle){
+    double angle = Enes100.getTheta();
+    int pref;
+
+
+    if((angle - newAngle > 0 && PI > angle - newAngle) || (angle - newAngle > -1*TWO_PI && -1*PI > angle - newAngle)){
+      
+      right_backwards();
+      left_forwards();
+    }
+    else{
+      right_forwards();
+      left_backwards();
+    }
+
+
+    while((newAngle - 0.3) > Enes100.getTheta() || Enes100.getTheta() > (newAngle + 0.3)){
+        analogWrite(BOTH_MOTORS_PWM,50);
+    }
+    //  while((newAngle - 0.01) > Enes100.getTheta() || Enes100.getTheta() > (newAngle + 0.01)){
+    //     Tank.setRightMotorPWM(10 * pref);
+    //     Tank.setLeftMotorPWM(-10 * pref);
+    //     angle = Enes100.getTheta();
+
+
+    // }
+    analogWrite(BOTH_MOTORS_PWM,0);
+    Enes100.println(Enes100.getTheta());
+}
+
+/*
+This function will stop moving automatically upon detecting an obstacle.
+*/
+void go_to_coords(float x, float y, int speed) {
+  float theta = atan((y-Enes100.getY())/(x-Enes100.getX()));
+  turn(theta);
+  drive_forward(speed);
+  while ( ( (Enes100.getX() != x) || (Enes100.getY() != y) ) && !is_obstacle())) {
+    delay(10);
+  }
+  stop_moving();
+}
+
 
 bool is_obstacle() {
   return read_distance() < 300.;
