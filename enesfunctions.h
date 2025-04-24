a9e00d4119be354e20b3ca5f980361d7e5957466
elias-commits <enelson6@terpmail.umd.edu>
2025-04-24 16:08:51 -0400

    Update enesfunctions.h
    Added all the code from this week

--- enesfunctions.h
+++ enesfunctions.h
@@ -14,6 +14,12 @@ struct RGB {
 
 };
 
+struct Point {
+  float x;
+  float y;
+  float theta;
+};
+
 // milimeters
 float read_distance() {
     digitalWrite(ULTRASONIC_TRIGGER,LOW);
@@ -151,27 +157,129 @@ void turn(double newAngle){
     Enes100.println(Enes100.getTheta());
 }
 
-/*
-This function will stop moving automatically upon detecting an obstacle.
-*/
-void go_to_coords(float x, float y, int speed) {
-  float theta = atan((y-Enes100.getY())/(x-Enes100.getX()));
-  turn(theta);
-  drive_forward(speed);
-  while ( ( (Enes100.getX() != x) || (Enes100.getY() != y) ) && !is_obstacle())) {
-    delay(10);
-  }
-  stop_moving();
-}
-
-
 bool is_obstacle() {
   return read_distance() < 300.;
 
 }
 
+
+/*
+
+This function will stop moving automatically upon detecting an obstacle.
+
+Do not use it for navigation that requires detecting obstacles
+
+*/
+void go_to_coords(float x, float y, int speed) {
+  // bug: navigating to a point with the same X coord will cause division by 0
+  float theta;
+  
+  if ((y != Enes100.getY()) && (x != Enes100.getX())) {
+    theta = atan((y-Enes100.getY())/(x-Enes100.getX()));
+  }
+  else if (x == Enes100.getX()) {
+    theta = ((y-Enes100.getY() > 0) - (y-Enes100.getY() < 0)) * HALF_PI; // pi/2 or -pi/2 depending on sign
+  }
+  else if (y == Enes100.getY()) {
+    theta = (x-Enes100.getX() > 0) ? 0 : -PI;
+  }
+  turn(theta);
+  drive_forwards(speed);
+  while ( ((Enes100.getX() != x) || (Enes100.getY() != y)) && !is_obstacle()) {
+    delay(10);
+  }
+  stop_moving();
+}
+
+void navigate_obstacles(float goal_x, float goal_y) {
+  go_to_coords(0.1, 1, 100);
+  float x, y;
+  float theta = Enes100.getTheta();
+  while ((x=Enes100.getX()) != goal_x || (y=Enes100.getY()) != goal_y) {
+    // might get stuck
+    // Liam's idea: obstacle counter??? there can only be 3
+
+    turn(0);
+    // turn up
+    if (is_obstacle()) {
+      turn(HALF_PI);
+    // turn down (we have reached the top)
+    }
+    if (is_obstacle()) {
+      turn(-HALF_PI);
+    }
+
+    // if neither condition is true, we will drive in the +x direction
+    // May need to modify for shorter checking intervals
+    drive_forwards(100);
+    delay(500);
+    stop_moving();
+  }
+
+}
+
+
 bool is_orzo() {
     struct RGB rgb;
     rgb = read_rgb();
     return ((rgb.r/rgb.g) < 1.21 );
+}
+
+
+// is 0 the correct value?
+void release_seed() {
+  trapdoorServo.write(0);
+}
+
+/*
+Assumes the OTV is already facing directly towards the plot and is in the correct position.
+Will probably need fine-tuning
+
+TODO: 
+- hard code the points by manually checking them
+*/
+void go_to_plot(int plot, bool top) {
+  plot += 4*top;
+  struct Point plots[] {
+    // bottom points
+    {0., 0., 0.},
+    {0., 0., 0.},
+    {0., 0., 0.},
+    {0., 0., 0.},
+    // top points
+    {0., 0., 0.},
+    {0., 0., 0.},
+    {0., 0., 0.},
+    {0., 0., 0.}  
+  };
+  go_to_coords(plots[plot].x, plots[plot].y,100);
+}
+
+void perform_mission(int iters) {
+  bool orzo_found;
+  bool top;
+  int plot = 0;
+  int counter;
+  // Navigate to mission site
+  top = Enes100.getY() > 1.;
+  if (top) {
+    go_to_coords(0.55,0.55,100);
+    turn(-HALF_PI);
+  }
+  else {
+    go_to_coords(0.55,1.45,100);
+    turn(HALF_PI);
+  }
+
+  while (!orzo_found) {
+    go_to_plot((++plot) % 4, top);
+    counter = 0;
+    for (int i = 0; i < iters; i++) {
+      counter += is_orzo();
+    }
+    orzo_found = (counter > iters / 2); 
+  }
+  //release_seed();
+  go_to_plot((++plot) % 4, top);
+  //collect_rocks();
 }
